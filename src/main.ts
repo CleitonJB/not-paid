cad518946a79c7f1a04d753c316788af26925926
Cleiton Braga <cleitonbraga56@gmail.com>
2024-01-19 22:55:01 -0300

    Refactor logic that apply fade to body element

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,7 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then(async () => {
     await setInitialOpacity();
-
-    const initialDate: Date = new Date('2024-01-16');
-    const daysDeadline: number = 60;
-    const dayMilliseconds : number = (1000 * 60 * 60 * 24);
-    let currentDate: Date = new Date();
-
-    let timeDifferentBetweenDates: number = (currentDate.getTime() - initialDate.getTime());
-    let daysDifferentBetweenDates: number = Math.round(timeDifferentBetweenDates / dayMilliseconds);
-
-    let bodyElement: HTMLBodyElement = document.getElementsByTagName('body')[0];
-
-    if(daysDifferentBetweenDates >= daysDeadline) {
-      bodyElement.style.opacity = '0.0';
-    } else {
-      const reduceFromOpacity: number = 0.0166666666667;
-      let newOpacity: number = (parseFloat(bodyElement.style.opacity) - (reduceFromOpacity * daysDifferentBetweenDates));
-
-      bodyElement.style.opacity = newOpacity.toString();
-    }
+    await setFade();
   })
   .catch(err => console.error(err));
 
@@ -41,4 +23,25 @@ async function setInitialOpacity(): Promise<void> {
       reject(error);
     }
   });
+}
+
+async function setFade(): Promise<void> {
+  const initialDate: Date = new Date('2024-01-16');
+  const daysDeadline: number = 60;
+  const dayMilliseconds: number = (1000 * 60 * 60 * 24);
+  let currentDate: Date = new Date();
+
+  let timeDifferentBetweenDates: number = (currentDate.getTime() - initialDate.getTime());
+  let daysDifferentBetweenDates: number = Math.round(timeDifferentBetweenDates / dayMilliseconds);
+
+  let bodyElement: HTMLBodyElement = document.getElementsByTagName('body')[0];
+
+  if(daysDifferentBetweenDates >= daysDeadline) {
+    bodyElement.style.opacity = '0.0';
+  } else {
+    const reduceFromOpacity: number = 0.0166666666667;
+    let newOpacity: number = (parseFloat(bodyElement.style.opacity) - (reduceFromOpacity * daysDifferentBetweenDates));
+
+    bodyElement.style.opacity = newOpacity.toString();
+  }
 }
